b52af84af81095307a1b7d35a183f93be2704330
youlhafi <youlhafiane@gmail.com>
2024-05-27 09:30:27 +0100

    work on splitting the layout in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './App.css'
+import SideBar from './components/SideBar/SideBar'
+import TopBar from './components/TopBar/TopBar'
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <SideBar/>
+      <main className='main'>
+        <TopBar/>
+        <div className='test'>
+          <div className='content'>
+            jojo
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
---
+++ frontend/src/components/TopBar/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TopBar.css'
+
+
+
+const TopBar = () => {
+  return (
+    <div className='topbar'>
+      topBar
+    </div>
+  )
+}
+
+export default TopBar
