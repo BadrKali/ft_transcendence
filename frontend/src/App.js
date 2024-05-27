38a39fdd77ccc37ff3b0416ea721b302185773cd
Badr Eddine El Kalai <115181341+BadrKali@users.noreply.github.com>
2024-05-27 10:54:38 +0100

    chore: Initialize functional components for pages and install React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,41 @@ import React from 'react'
 import './App.css'
 import SideBar from './components/SideBar/SideBar'
 import TopBar from './components/TopBar/TopBar'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import DashBoard from './pages/Dashboard/DashBoard';
+import Game from './pages/Game/Game';
+import Chat from './pages/Chat/Chat';
+import Tournament from './pages/Tournament/Tournament';
+import LeaderBoard from './pages/LeaderBoard/LeaderBoard';
+import Setting from './pages/Setting/Setting';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <DashBoard/>
+    },
+    {
+      path: '/game',
+      element: <Game />
+    },
+    {
+      path: '/Chat',
+      element: <Chat />
+    },
+    {
+      path: '/Tournament',
+      element: <Tournament />
+    },
+    {
+      path: '/LeaderBoard',
+      element: <LeaderBoard />
+    },
+    {
+      path: '/Setting',
+      element: <Setting />
+    },
+  ])
   return (
     <div className='app'>
       <SideBar/>
@@ -12,12 +44,16 @@ const App = () => {
         <TopBar/>
         <div className='page-content'>
           <div className='content'>
-            hellow
+            <RouterProvider router={router}/> 
           </div>
         </div>
       </main>
     </div>
   )
 }
+
+
+
+
 
 export default App
