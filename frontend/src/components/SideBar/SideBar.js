7f7b36b50a1a91f00f6d2210ca721192a7e6ffb2
Badr Eddine El Kalai <115181341+BadrKali@users.noreply.github.com>
2024-05-28 13:05:46 +0100

    feat(sidebar): implement sidebar layout

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SideBar.css'
+import { assets } from '../../assets/assets'
+import { Link }  from 'react-router-dom'
+import Icon from '../../assets/Icon/icons'
+import { SideBarData } from './SideBarData'
 
 const SideBar = () => {
+  const [menuActive, setMenuActive] = useState('Dashboard')
+
+  function handleMenuClick(item) {
+    setMenuActive(item)
+  }
+
   return (
-    <div className='sidebar'>SideBar</div>
+    <div className='sidebar-container'>
+      <div className='sidebar-logo-container'>
+        <img src={assets.logo} alt='Pongy Logo'/>
+      </div>
+        <nav className='nav-container'>
+          <ul className='nav-menu-items'>
+            {SideBarData.map((item, index) => {
+              return(
+                <li key={index} className={`navbar-toggle ${menuActive === item.title ? 'active' : ''}`} onClick={()=>handleMenuClick(item.title)}>
+                  <Link to={item.path} className='navbar-toggle-link' >
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })}
+            <li className='navbar-toggle-logout'>
+              <div className='logout-sep'>
+                <img src={assets.sep}/>
+              </div>
+              <Link to='/logout' className='logout-link'>
+                <Icon name='logout' className='navbar-toggle-icon'/>
+                <span>Logout</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+    </div>
   )
 }
 
---
+++ frontend/src/components/SideBar/SideBarData.js
@@ -0,0 +1,30 @@
+import Icon from '../../assets/Icon/icons'
+
+
+export const SideBarData = [
+    {
+        title: 'Dashboard',
+        path: '/',
+        icon: <Icon name='dashboard' className='navbar-toggle-icon'/>,
+    },
+    {
+        title: 'Game',
+        path: '/game',
+        icon: <Icon name='game' className='navbar-toggle-icon'/>,
+    },
+    {
+        title: 'Chat',
+        path: '/chat',
+        icon: <Icon name='chat' className='navbar-toggle-icon'/>,
+    },
+    {
+        title: 'Tournament',
+        path: '/tournament',
+        icon: <Icon name='tournament' className='navbar-toggle-icon'/>,
+    },
+    {
+        title: 'Setting',
+        path: '/setting',
+        icon: <Icon name='setting' className='navbar-toggle-icon'/>,
+    }
+]
