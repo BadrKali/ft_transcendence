17d282c0799290ccf7d880fc9eeaa2df93a072f1
Mohamed HABIB ALLAH <100409109+mohamedhabiballah1@users.noreply.github.com>
2024-05-30 13:13:57 +0100

    chore: create the games area

--- frontend/src/pages/Game/Game.js
+++ frontend/src/pages/Game/Game.js
@@ -1,10 +1,24 @@
 import React from 'react'
+// import pingPongComponent from './components/pingPongMainComponents'
+import './game.css'
+import pingLogo from './Game-assets/ping.svg'
+import XOLogo from './Game-assets/XO.svg'
 
 const Game = () => {
   return (
     <div className='game-container'>
         <div className='page-title'>
-          <h1>Game</h1>
+          <h1>Game Lobby</h1>
+          <div className="gameArea">
+            <div className="ping-pong">
+                <img src={pingLogo} className='gameLogo' alt=''/>
+                <p className='gameTitle'>Ping Pong</p>
+            </div>
+            <div className="XO">
+              <img src={XOLogo} className='gameLogo' alt=''/>
+              <p className='gameTitle'>XO Game</p>
+            </div>
+          </div>
         </div>
     </div>
   )
