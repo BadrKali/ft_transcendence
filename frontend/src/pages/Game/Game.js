f2baeb48e9b3bcddf451a8141635f7d044d16a08
Mohamed HABIB ALLAH <100409109+mohamedhabiballah1@users.noreply.github.com>
2024-05-30 13:30:55 +0100

    feat: add hover border and selection state to games components

--- frontend/src/pages/Game/Game.js
+++ frontend/src/pages/Game/Game.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 // import pingPongComponent from './components/pingPongMainComponents'
 import './game.css'
 import pingLogo from './Game-assets/ping.svg'
 import XOLogo from './Game-assets/XO.svg'
 
 const Game = () => {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  const handleSelectGame = (game) => {
+    setSelectedGame(game);
+  };
+
   return (
     <div className='game-container'>
         <div className='page-title'>
           <h1>Game Lobby</h1>
           <div className="gameArea">
-            <div className="ping-pong">
+            <div className={`ping-pong gameComponent ${selectedGame === 'ping-pong' ? 'selected' : ''}`}
+        onClick={() => handleSelectGame('ping-pong')}>
                 <img src={pingLogo} className='gameLogo' alt=''/>
                 <p className='gameTitle'>Ping Pong</p>
             </div>
-            <div className="XO">
+            <div className={`XO gameComponent ${selectedGame === 'XO' ? 'selected' : ''}`}
+        onClick={() => handleSelectGame('XO')}>
               <img src={XOLogo} className='gameLogo' alt=''/>
               <p className='gameTitle'>XO Game</p>
             </div>
