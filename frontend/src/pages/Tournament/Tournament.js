1cb6404344fc64ef9421e5ecf7317e84792c6567
Badr Eddine El Kalai <bel-kala@student.1337.ma>
2024-05-31 09:09:10 +0100

    feat(Tournament): Implement navigation menu with state management

--- frontend/src/pages/Tournament/Tournament.js
+++ frontend/src/pages/Tournament/Tournament.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Tournament.css'
+import Icon from '../../assets/Icon/icons'
+
 
 const Tournament = () => {
+
+
+  const [tournamentMenuItem, setTournamentMenuItem] = useState('overview')
   return (
     <div className='tournment-container'>
       <div className='page-title'>
           <h1>Tournament</h1>
+      </div>
+      <div className='tournament-menu'>
+        <div className={`tournament-menu-item ${tournamentMenuItem === 'overview' ? 'active-tournament-menu' : ''}`} onClick={() => {
+          setTournamentMenuItem('overview')
+        }}>
+          <Icon name='view' className='tournament-icon' />
+          <span>Overview</span>
         </div>
+        <div className={`tournament-menu-item ${tournamentMenuItem === 'bracket' ? 'active-tournament-menu' : ''}`} onClick={() => {
+          setTournamentMenuItem('bracket')
+        }}>
+          <Icon name='bracket' className='tournament-icon'/>
+          <span>Bracket</span>
+        </div>
+        <div className={`tournament-menu-item ${tournamentMenuItem === 'logs' ? 'active-tournament-menu' : ''}`} onClick={() => {
+          setTournamentMenuItem('logs')
+        }}>
+          <Icon name='history' className='tournament-icon'/>
+          <span>Logs</span>
+        </div>
+      </div>
     </div>
   )
 }
