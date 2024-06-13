156c4b48f769c50a26a55459987cc222f7959f48
youlhafi <youlhafiane@gmail.com>
2024-06-13 12:01:27 +0100

    feat(backend): add model of achievement and playerAchievments

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import GameHistory
+from .models import GameHistory, Achievement, PlayerAchievement
+from user_management.serializers import PlayerSerializer
 
 class GameHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = GameHistory
         fields = '__all__'
+
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = ['id', 'title', 'description', 'image', 'task']
+
+
+class PlayerAchievementSerializer(serializers.ModelSerializer):
+    player = serializers.PrimaryKeyRelatedField(read_only=True)
+    achievement = AchievementSerializer()
+
+    class Meta:
+        model = PlayerAchievement
+        fields = ['player', 'achievement', 'achieved_at']
