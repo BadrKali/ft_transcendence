a4910077a5a64f223554b1495908871d7db63ff9
Badr Eddine El Kalai <bel-kala@student.1337.ma>
2024-07-10 22:04:03 +0100

    fix(backend) : update profile view and resolve errors
    - Improved profile update view
    - Fixed validation
    - Enhanced error handling

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -12,6 +12,44 @@ class CurrentUserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'avatar', 'username', 'email', 'avatar_type']
 
+
+class CurrentUserSettingsSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True, required=False)
+    new_password = serializers.CharField(write_only=True, required=False)
+    avatar_type = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'avatar', 'email', 'old_password', 'new_password', 'avatar_type']
+        extra_kwargs = {
+            'username': {'required': False},
+            'email': {'required': False},
+            'avatar': {'required': False},
+        }
+    def validate(self, data):
+        user = self.context['request'].user
+        if ('avatar_type' in data) and ('avatar' in data):
+            raise serializers.ValidationError({"avatar": "there is 2 avatars provided ghayerha"})
+        if 'new_password' in data:
+            if not user.check_password(data.get('old_password')):
+                raise serializers.ValidationError({"old_password": "Old password is not correct"})
+            if data['new_password'] == data['old_password']:
+                raise serializers.ValidationError({"new_password": "New password cannot be the same as the old password"})
+        return data
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+        instance.email = validated_data.get('email', instance.email)
+        if 'new_password' in validated_data:
+            instance.set_password(validated_data['new_password'])
+        if 'avatar_type' in validated_data:
+            avatar_type = validated_data.get('avatar_type')
+            instance.avatar = f'avatars/{avatar_type}.png'
+        instance.save()
+        return instance
+
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True, required=True)
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
