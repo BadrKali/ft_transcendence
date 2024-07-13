2b1d2dac2b8e929cf3e506673863026dd70a8320
Mohamed HABIB ALLAH <100409109+mohamedhabiballah1@users.noreply.github.com>
2024-07-13 02:00:23 +0100

    feat(Backend/Game): add an implementation for GameConsumer

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,5 +1,7 @@
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from .models import GameRoom
 
 class TestConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -19,4 +21,51 @@ class TestConsumer(AsyncWebsocketConsumer):
         achievement = event["achievement"]
         await self.send(text_data=json.dumps({
             "achievement": achievement
+        }))
+
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass 
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        action = data.get('action')
+        if action == 'random':
+            await self.handle_random_mode()
+
+    async def handle_random_mode(self):
+        room = await sync_to_async(self.find_or_create_room)()
+        if room.player1 == self.user:
+            await self.send(text_data=json.dumps({
+                'message': 'Waiting for another player...'
+            }))
+        else:
+            await self.channel_layer.group_send(
+                f"room_{room.id}",
+                {
+                    'type': 'start_game',
+                    'room_id': room.id
+                }
+            )
+
+    @sync_to_async
+    def find_or_create_room(self):
+        room = GameRoom.objects.filter(is_waiting=True).first()
+        if room:
+            room.player2 = self.user
+            room.is_waiting = False
+            room.save()
+        else:
+            room = GameRoom.objects.create(player1=self.user)
+        return room
+
+    async def start_game(self, event):
+        room_id = event['room_id']
+        await self.send(text_data=json.dumps({
+            'action': 'start_game',
+            'room_id': room_id,
         }))
