924cb1ab279d13c5f32a2f4e3f8d04b5c424e44b
Badr Eddine El Kalai <bel-kala@student.1337.ma>
2024-07-22 16:14:33 +0100

    feat(backend): Implement Friend Request Management and Friend Management Endpoints
    * Add FriendRequestManagementView to handle sending and checking friend requests
    * Add FriendRequestResponseView to handle accepting and rejecting friend requests
    * Add FriendManagementView to handle checking friendship status and unfriending users
    * Refactor filtering logic to use Q objects for cleaner and more efficient queries
    * Update URL configurations to include new endpoints

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path,include
-from .views import FriendsListView, CreateFriendshipView, DeleteFriendshipView,BlockFriendView, UnblockFriendView,PlayerView, FrindInvitationsView, SearchAPIView, CurrentFriendsListView, OtherPlayerView
+from .views import *
 
 urlpatterns = [
-    path('friends/create/<int:friend_id>/', CreateFriendshipView.as_view(), name='create_friendship'),
-    path('friends/delete/<int:player_id>/<int:friend_id>/', DeleteFriendshipView.as_view(), name='delete_friendship'),
+
+    path('friends-request/<int:receiver_id>/', FriendRequestManagementView.as_view(), name='manage_friend_request_detail'),
+    path('friends-request/<int:sender_id>/response/', FriendRequestResponse.as_view(), name='friend_request_response'),
+    path('friend/<int:friend_id>', FriendManagementView.as_view(), name='friend_management'),
+    path('<int:friend_id>/block-unblock', BlockUnblockView.as_view(), name='bBlockUnblock_users'),
+
+
+
+    # path('friends/create/<int:friend_id>/', CreateFriendshipView.as_view(), name='create_friendship'),
+    # path('friends/delete/<int:player_id>/<int:friend_id>/', DeleteFriendshipView.as_view(), name='delete_friendship'),
     path('friends/list/<int:player_id>/', FriendsListView.as_view(), name='friends_list'),
     path('friends/list/me/', CurrentFriendsListView.as_view(), name='friends_list'),
-    path('friends/block/<int:player_id>/<int:friend_id>/', BlockFriendView.as_view(), name='block_friend'),
-    path('friends/unblock/<int:player_id>/<int:friend_id>/', UnblockFriendView.as_view(), name='unblock_friend'),
+    # path('friends/block/<int:player_id>/<int:friend_id>/', BlockFriendView.as_view(), name='block_friend'),
+    # path('friends/unblock/<int:player_id>/<int:friend_id>/', UnblockFriendView.as_view(), name='unblock_friend'),
     path('stats/', PlayerView.as_view(), name='player_view'),
     path('stats/<int:player_id>', OtherPlayerView.as_view(), name='player_view'),
-    path('friends/invitations/', FrindInvitationsView.as_view(), name='player_invitations'),
     # path('player/', PlayerView.as_view(), name='player_view'),
     path('search/', SearchAPIView.as_view(), name='search-api'),
 ]
--- backend/user_management/views.py
+++ backend/user_management/views.py
@@ -12,39 +12,129 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.db.utils import IntegrityError
 from game.models import Achievement
 from game.serializers import AchievementSerializer
+from .serializers import FriendInvitation
 from django.conf import settings
 from authentication .models import User
 from authentication .serializers import CurrentUserSerializer
+from django.db.models import Q
 
 
-
-class CreateFriendshipView(APIView):
-    def post(self, request, friend_id):
-        player = request.user  
-        friend = get_object_or_404(User, id=friend_id) 
+class FriendRequestManagementView(APIView):
+    
+    def get_friendship_status(self, player_sender, player_receiver):
+        return FriendInvitation.objects.filter(
+                                                Q(player_sender=player_sender, player_receiver=player_receiver) |
+                                                Q(player_sender=player_receiver, player_receiver=player_sender)).exists()
+    
+    def get(self, request, receiver_id):
+        player_sender = request.user
+        player_receiver = get_object_or_404(User, id=receiver_id)
+        request_exists = self.get_friendship_status(player_sender, player_receiver)
+        if request_exists:
+            return Response({"message": "Friend request exists."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Friend request does not exist."}, status=status.HTTP_200_OK)
+    
+    def post(self, request, receiver_id):
+        player_sender = request.user
+        player_receiver = get_object_or_404(User, id=receiver_id)
         
-        friendship = Friendship(player=player, friend=friend) 
+        if self.get_friendship_status(player_sender, player_receiver):
+            return Response({'error': 'Friend request already exists.'}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            friendship.full_clean()  
-            friendship.save()
-            return Response({"message": "Friendship created successfully."}, status=status.HTTP_201_CREATED)
+            friend_request = FriendInvitation(player_sender=player_sender, player_receiver=player_receiver)
+            friend_request.save()
+            return Response({'message': 'Friend request sent successfully.'}, status=status.HTTP_201_CREATED)
         except ValidationError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, receiver_id):
+        player_sender = request.user
+        player_receiver = get_object_or_404(User, id=receiver_id)
 
-
-class DeleteFriendshipView(APIView):
-
-    def delete(self, request, player_id, friend_id):
         try:
-            friendship = Friendship.objects.get(player_id=player_id, friend_id=friend_id)
-            reverse_friendship = Friendship.objects.get(player_id=friend_id, friend_id=player_id)
+            invitation = get_object_or_404(FriendInvitation, player_sender=player_sender, player_receiver=player_receiver)
+            invitation.delete()
+            return Response({'message': 'Friend request canceled.'}, status=status.HTTP_202_ACCEPTED)
+        except FriendInvitation.DoesNotExist:
+            return Response({'error': 'Friend request not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+
+
+
             
+class FriendRequestResponse(APIView):
+    def patch(self, request, sender_id):
+        player_sender = get_object_or_404(User, id=sender_id)
+        player_receiver = request.user
+        friend_request = get_object_or_404(FriendInvitation, player_sender=player_sender, player_receiver=player_receiver)
+        if friend_request.invite_status != 'P':
+            return Response({'error': 'Invalid status.'}, status=status.HTTP_400_BAD_REQUEST)
+        action = request.data.get('status')
+        if action not in ['accept', 'reject']:
+            return Response({'error': 'Invalid action. Choose "accept" or "reject".'}, status=status.HTTP_400_BAD_REQUEST)
+        if action == 'accept':
+            friend_request.invite_status = 'A'
+            friendship = Friendship(player=player_sender, friend=player_receiver)
+            friendship.save()
+            friend_request.save()
+            return Response({'message': 'Friend request accepted.'}, status=status.HTTP_200_OK)
+        elif action == 'reject':
+            return Response({'message': 'Friend request rejected.'}, status=status.HTTP_200_OK)
+
+
+class FriendManagementView(APIView):
+    def get(self, request, friend_id):
+        currentUser = request.user
+        friend = get_object_or_404(User, id=friend_id)
+        is_friend = Friendship.objects.filter(Q(player=currentUser, friend=friend) | Q(player=friend, friend=currentUser)).exists()
+        if is_friend:
+            return Response({"message": f"{friend} is a friend."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": f"{friend} is not a friend."}, status=status.HTTP_404_NOT_FOUND)
+        
+    def detete(self, request, friend_id):
+        currentUser = request.user
+        friend = friend = get_object_or_404(User, id=friend_id)
+        friendship = Friendship.objects.filter(Q(player=currentUser, friend=friend) | Q(player=friend, friend=currentUser))
+        if friendship.exists():
             friendship.delete()
-            reverse_friendship.delete()
-            
-            return Response({"message": "Friendship deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
-        except Friendship.DoesNotExist:
+            return Response({"message": f"Successfully unfriended {friend}."}, status=status.HTTP_202_ACCEPTED)
+        else:
             return Response({"error": "Friendship does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+
+class BlockUnblockView(APIView):
+    pass
+
+
+# class CreateFriendshipView(APIView):
+#     def post(self, request, friend_id):
+#         player = request.user  
+#         friend = get_object_or_404(User, id=friend_id) 
+        
+#         friendship = Friendship(player=player, friend=friend) 
+#         try:
+#             friendship.full_clean()  
+#             friendship.save()
+#             return Response({"message": "Friendship created successfully."}, status=status.HTTP_201_CREATED)
+#         except ValidationError as e:
+#             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class DeleteFriendshipView(APIView):
+
+#     def delete(self, request, player_id, friend_id):
+#         try:
+#             friendship = Friendship.objects.get(player_id=player_id, friend_id=friend_id)
+#             reverse_friendship = Friendship.objects.get(player_id=friend_id, friend_id=player_id)
+            
+#             friendship.delete()
+#             reverse_friendship.delete()
+            
+#             return Response({"message": "Friendship deleted successfully."}, status=status.HTTP_202_ACCEPTED)
+#         except Friendship.DoesNotExist:
+#             return Response({"error": "Friendship does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
 class CurrentFriendsListView(generics.ListAPIView):
     serializer_class = FriendshipSerializer
@@ -61,38 +151,31 @@ class FriendsListView(generics.ListAPIView):
         player_id = self.kwargs['player_id']
         return Friendship.objects.filter(player_id=player_id)
 
-class BlockFriendView(APIView):
-    def post(self, request, player_id, friend_id):
-        player = get_object_or_404(Player, id=player_id)
-        friend = get_object_or_404(Player, id=friend_id)
-        try:
-            friendship = Friendship.objects.get(player=player, friend=friend)
-            friendship.blocked = True
-            friendship.save()
-            return Response({'status': 'friend blocked'}, status=status.HTTP_200_OK)
-        except Friendship.DoesNotExist:
-            return Response({'error': 'friendship does not exist'}, status=status.HTTP_404_NOT_FOUND)
+# class BlockFriendView(APIView):
+#     def post(self, request, player_id, friend_id):
+#         player = get_object_or_404(Player, id=player_id)
+#         friend = get_object_or_404(Player, id=friend_id)
+#         try:
+#             friendship = Friendship.objects.get(player=player, friend=friend)
+#             friendship.blocked = True
+#             friendship.save()
+#             return Response({'status': 'friend blocked'}, status=status.HTTP_200_OK)
+#         except Friendship.DoesNotExist:
+#             return Response({'error': 'friendship does not exist'}, status=status.HTTP_404_NOT_FOUND)
         
 
-class UnblockFriendView(APIView):
-    def post(self, request,player_id, friend_id):
-        player = get_object_or_404(Player, id=player_id)
-        friend = get_object_or_404(Player, id=friend_id)
-        try:
+# class UnblockFriendView(APIView):
+#     def post(self, request,player_id, friend_id):
+#         player = get_object_or_404(Player, id=player_id)
+#         friend = get_object_or_404(Player, id=friend_id)
+#         try:
 
-            friendship = Friendship.objects.get(player=player, friend=friend)
-            friendship.blocked = False
-            friendship.save()
-            return Response({'status': 'friend unblocked'}, status=status.HTTP_200_OK)
-        except Friendship.DoesNotExist:
-            return Response({'error': 'friendship does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-
-
-
-
-
-
+#             friendship = Friendship.objects.get(player=player, friend=friend)
+#             friendship.blocked = False
+#             friendship.save()
+#             return Response({'status': 'friend unblocked'}, status=status.HTTP_200_OK)
+#         except Friendship.DoesNotExist:
+#             return Response({'error': 'friendship does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
  
 class PlayerView(APIView):
@@ -122,17 +205,6 @@ class GamePlayersView(APIView):
         player = get_object_or_404(Player, user_id=player_id)
         serializer = PlayerSerializer(player)
         return(Response(serializer.data, status=status.HTTP_200_OK))
-
-
-
-class FrindInvitationsView(APIView):
-    def get(self, request):
-        invitation = FriendInvitation.objects.filter(player_receiver=request.user)
-        serializer = FriendInvitationsSerializer(invitation, many=True)
-        return(Response(serializer.data, status=status.HTTP_200_OK))
-    
-
-
 
 
 # test for search 
