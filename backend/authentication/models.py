aa7fc8faedd37ab5bab787dcec4482c8efc81252
Badr Eddine El Kalai <bel-kala@student.1337.ma>
2024-07-28 15:22:30 +0100

    feat(backend) : Implement WebSocket consumer for real-time notifications and friends' status updates

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     api_42_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    
+    is_online = models.BooleanField(default=False)
     def save(self, *args, **kwargs):
         created = self.pk is None
         super().save(*args, **kwargs)
--- backend/user_management/consumers.py
+++ backend/user_management/consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from channels.db import database_sync_to_async
+
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         if self.scope['user'].is_anonymous:
@@ -11,25 +13,76 @@ class NotificationConsumer(AsyncWebsocketConsumer):
                 self.room_group_name,
                 self.channel_name
             )
+            await self.set_user_status(True)
+            await self.broadcast_status_update(True)
             await self.accept()
 
     async def disconnect(self, close_code):
+        await self.set_user_status(False)  
+        await self.broadcast_status_update(False) 
+        
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     async def receive(self, content):
-        message = content['message']
+        pass
+        # message = content['message']
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
+
+    @database_sync_to_async
+    def set_user_status(self, is_online):
+        self.user.is_online = is_online
+        self.user.save()
+
+    async def broadcast_status_update(self, online):
+        friends_ids = await self.get_user_friends()
+        status = 'online' if online else 'offline'
+        for friend_id in friends_ids: 
+            group_name = f'notifications_{friend_id}'
+            await self.channel_layer.group_send(
+                group_name,
+                {
+                    'type': 'status_update',
+                    'user_id': self.user.id,
+                    'status': status
+                }
+            )
+
+    async def status_update(self, event):
+        user_id = event['user_id']
+        status = event['status']
         await self.send(text_data=json.dumps({
-            'message': message
+            'type': 'status_update',
+            'user_id': user_id,
+            'status': status
         }))
+
+        
+
+    @database_sync_to_async
+    def get_user_friends(self):
+        from .models import Friendship
+        friendships = Friendship.objects.filter(player=self.user)
+        friends_ids = friendships.values_list('friend_id', flat=True)
+        return list(friends_ids)
 
     async def notification_message(self, event):
         message = event['message']
         await self.send(text_data=json.dumps({
+            'type' : 'notification',
             'message': message
         }))
+
+
+
+
+
+
+
 
 class EchoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -48,3 +101,103 @@ class EchoConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': text_data
         }))
+
+
+
+
+
+# from channels.generic.websocket import AsyncWebsocketConsumer
+# import json
+# from channels.db import database_sync_to_async
+
+
+# class NotificationConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         if self.scope['user'].is_anonymous:
+#             await self.close()
+#         else:
+#             self.user = self.scope['user']
+#             self.room_group_name = f'notifications_{self.user.id}'
+#             await self.channel_layer.group_add(
+#                 self.room_group_name,
+#                 self.channel_name
+#             )
+#             await self.set_user_status(True)
+#             await self.broadcast_status_update(True)
+#             await self.accept()
+
+#     async def disconnect(self, close_code):
+#         await self.set_user_status(False)
+#         await self.broadcast_status_update(True)
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#     async def receive(self, content):
+#         message = content['message']
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+
+#     @database_sync_to_async
+#     def set_user_status(self, is_online):
+#         self.user.is_online = is_online
+#         self.user.save()
+
+
+#     async def broadcast_status_update(self, online):
+#         friends = self.get_user_friends()
+#         status = 'online' if online else 'offline'
+#         for friend in friends:
+#             group_name = f'notifications_{friend.id}'
+#             await self.channel_layer.group_send(
+#                 group_name,
+#                 {
+#                     'type': 'status_update',
+#                     'user_id': self.user.id,
+#                     'status': status
+#                 }
+#             )
+
+#     async def notification_message(self, event):
+#         message = event['message']
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+
+#     async def status_update(self, event):
+#         user_id = event['user_id']
+#         status = event['status']
+#         await self.send(text_data=json.dumps({
+#             'type': 'status_update',
+#             'user_id': user_id,
+#             'status': status
+#         }))
+
+#     @database_sync_to_async
+#     def get_user_friends(self):
+#         from .models import Friendship
+#         friendships = Friendship.objects.filter(player=self.user)
+#         friends_ids = friendships.values_list('friend_id', flat=True)
+#         return friends_ids
+
+
+
+# class EchoConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         if self.scope['user'].is_anonymous:
+#             await self.close()
+#         else:
+#             await self.accept()
+            
+#             print(f"User connected: {self.scope['user']}")
+
+#     async def disconnect(self, close_code):
+#         pass
+
+#     async def receive(self, text_data):
+#         print(text_data)
+#         await self.send(text_data=json.dumps({
+#             'message': text_data
+#         }))
