4ef902cbc3b2c5e2245821480dacc57960e8b68b
ABDELALI AIT TALB <aittalbabdelalie@gmail.com>
2024-07-29 14:39:47 +0100

    Fix: (Backend/Chat): CreatedAt type is changed From :TimeField  To DateTimeField
    was caused an issue when retreiving last Message

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -16,22 +16,15 @@ def format_date(date)-> str:
 @api_view(['GET'])
 def RetreiveContacts(request):
     Mycontacts = message.objects.filter(Q(sender_id=request.user.id) | Q(receiver_id=request.user.id));
-
     serializers = __messageSerializer__(Mycontacts, many=True)
-
     evaluatedData = serializers.data
-
     contactIDs = {elem.get('receiver_id') if elem.get('receiver_id') != request.user.id else elem.get('sender_id') for elem in evaluatedData}
-
     contacts = User.objects.filter(id__in=contactIDs)
-
     ContactSerializer = __user_serializer__(contacts, many=True)
-
     ChatList = [{ **contact,
                   "unreadMessages": message.UnreadMessageBeetwen(request.user.id, contact["id"]),
                   "lastMessage": message.getLastMessage(request.user.id, contact["id"]).content,
                   "lastTime": format_date(message.getLastMessage(request.user.id, contact["id"]).created_at),
                 } for contact in ContactSerializer.data 
                 ]
- 
     return Response(ChatList)
