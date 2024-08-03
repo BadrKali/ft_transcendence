48e4fc9404fabe5a47684099c918a1cf4dff3523
Mohamed HABIB ALLAH <habibsimo692@gmail.com>
2024-08-03 09:16:51 +0100

    feat(Frontend/Game): fetch the friends list in the invite mode

--- frontend/src/pages/Game/components/PingPong/PlayerSelection.js
+++ frontend/src/pages/Game/components/PingPong/PlayerSelection.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import players from '../../Data';
 import defit from '../../Game-assets/defit.png'
+import useFetch from '../../../../hooks/useFetch';
+import useAuth from '../../../../hooks/useAuth';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const WS_BACKEND_URL = process.env.REACT_APP_WS_BACKEND_URL;
+
 const PlayerSelection = ({ onPlayerSelect, onCancel }) => {
+  const {data ,isLoading, error} = useFetch(`${BACKEND_URL}/user/friends/list/`)
   return (
     <div className="player-selection-overlay">
         <div className="player-selection">
             <h2>ONLINE FRIENDS</h2>
             <ul>
-                {players.map(player => (
+                {data && data.map(player => (
                 <li key={player.id} >
-                    <img src={player.picture} alt={player.name} className='player-image'/>
+                    <img src={player.avatar} alt={player.username} className='player-image'/>
                     <div className='player-info'>
-                    <p className='player-name'>{player.name}</p>
+                    <p className='player-name'>{player.username}</p>
                     <p className='player-rank'>Rank: {player.rank}</p>
                     </div>
                     <button className='player-challenge' onClick={() => onPlayerSelect(player)}><img src={defit} alt=""/></button>
