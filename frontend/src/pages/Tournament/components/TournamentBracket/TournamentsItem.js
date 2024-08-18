5f5e6916f4c383a7a61e15671b72ecab35e29cfe
youlhafi <youlhafiane@gmail.com>
2024-08-18 10:35:01 +0100

    feat: add unknown avatars to tournament brackets

--- frontend/src/pages/Tournament/components/TournamentBracket/TournamentsItem.js
+++ frontend/src/pages/Tournament/components/TournamentBracket/TournamentsItem.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import './TournamentsItem.css'
+import { avatars } from '../../../../assets/assets';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 function TournamentsItem({players}) {
-  console.log(players.player1.avatar);
+  const unknownAvatar = avatars[4].img;
+  console.log(players)
   return (
     <div className="tournamentsItem-match-container">
       <div className={players.win ? 'tournamentsItem-container wone' : 'tournamentsItem-container losse'}>
           <div className="player-image">
-              <img src={`${BACKEND_URL}${players.player1.avatar}`}/>
+            <img src={players.player1?.avatar ? `${BACKEND_URL}${players.player1.avatar}` : unknownAvatar} alt={players.player1?.username || 'Unknown Player'} />
           </div>
           <div className="player-name">
-            <p>{players.player1.username}</p>
+            <p>{players.player1?.username || 'Unknown Player'}</p>
           </div>
       </div>
       <div className={players.win ? 'tournamentsItem-container wone' : 'tournamentsItem-container losse'}>
         <div className="player-image">
-          <img src={`${BACKEND_URL}${players.player2.avatar}`}/>
+            <img src={players.player2?.avatar ? `${BACKEND_URL}${players.player2.avatar}` : unknownAvatar} alt={players.player2?.username || 'Unknown Player'} />
 
         </div>
         <div className="player-name">
-          <p>{players.player2.username}</p>
+          <p>{players.player2?.username || 'Unknown Player'}</p>
         </div>
         </div>
     </div>
