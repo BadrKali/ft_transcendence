e04f020c2e8593959e1e47697d9c1e57d411c4f1
Mohamed HABIB ALLAH <habibsimo692@gmail.com>
2024-10-01 12:46:37 +0100

    feat(Game): set default game settings

--- backend/game/models.py
+++ backend/game/models.py
@@ -61,9 +61,9 @@ class UserAchievement(models.Model):
 
 class GameSettings(models.Model):
     user = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True, null=True)
-    background = models.CharField(max_length=255)
-    paddle = models.CharField(max_length=255)
-    keys = models.CharField(max_length=255, null=True)
+    background = models.CharField(max_length=255, default="hell")
+    paddle = models.CharField(max_length=255, default="#BC4F00")
+    keys = models.CharField(max_length=255, default="up-down")
     gameMode = models.CharField(max_length=255, default="flesh")
     def __str__(self):
         return f"{self.background} {self.paddle} {self.gameMode}"
