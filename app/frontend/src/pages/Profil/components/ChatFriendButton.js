d74ffaa5858f45d3fd922a184a351b7ffa61852e
Ahmed Fatir <afatir@e3r1p17.1337.ma>
2024-11-14 17:13:11 +0100

    new architecture

--- app/frontend/src/pages/Profil/components/ChatFriendButton.js
+++ app/frontend/src/pages/Profil/components/ChatFriendButton.js
@@ -26,7 +26,7 @@ const ChatFriendButton = ({profilData: ChatPartnerData}) => {
             clientSocket?.send(JSON.stringify({type: '_start_chat_', 
                 messageData : ChatPartnerData
             }))
-        }, 1111)
+        }, 5555)
 
     };
 
